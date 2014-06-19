5a27eb0018875b22bd1a99218ce9ec5d908de4da
Andrew Weller <Andrew.Weller@soteradefense.com>
2014-06-19 13:18:29 -0400

    Modifed BuildAll.bat to build online by default.  Included BuildAll-offline.bat to build offline.

--- graphene-parent/BuildAll-offline.bat
+++ graphene-parent/BuildAll-offline.bat
@@ -3,4 +3,4 @@ echo Cleaning all target directories, twice. Sometimes Windows holds onto file h
 echo.
 echo Run 'mvn clean' by itself if there is a failure in cleaning, and make sure no files in any of the target directories is open or being used.
 
-mvn clean -Dmaven.clean.failOnError=false && mvn clean -Dmaven.clean.failOnError=false && mvn install -DskipTests=true
+mvn clean -Dmaven.clean.failOnError=false && mvn clean -Dmaven.clean.failOnError=false && mvn install -DskipTests=true -o
