93fd05a9120d4606298aefaab493e824824f8022
Eli Fajardo <elifajardo@gmail.com>
2014-08-03 12:38:08 -0400

    Removed unnecessary repos in graphene-parent POM to allow for faster jar downloads and build. Also commented out line in NetworkUtilsTest that tests 0.0.0.0:135. This is specific to Windows svchost and will cause build fail in non-Windows environment.

--- graphene-parent/graphene-util/src/test/java/graphene/util/net/NetworkUtilsTest.java
+++ graphene-parent/graphene-util/src/test/java/graphene/util/net/NetworkUtilsTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class NetworkUtilsTest {
 	@Test
 	public void test1() {
-		AssertJUnit.assertTrue(NetworkUtils.isServerAlive("0.0.0.0", 135));
+		//AssertJUnit.assertTrue(NetworkUtils.isServerAlive("0.0.0.0", 135));
 		//AssertJUnit.assertFalse(NetworkUtils.isServerAliveOn80("localhost"));
 		//AssertJUnit.assertFalse(NetworkUtils.isServerAliveOn80("127.0.0.1"));
 	}
