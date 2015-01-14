74f979f3f4d5555267e810a47e3a480223d9cdc7
Andrew Weller <Andrew.Weller@soteradefense.com>
2015-01-14 09:35:23 -0500

    WORK-IN-PROGRESS: the report amounts are formatted like currency.  However, when sorting on that column, the values are treated like a string and not sorted like a numerical value

--- graphene-parent/graphene-util/src/main/java/graphene/util/DataFormatConstants.java
+++ graphene-parent/graphene-util/src/main/java/graphene/util/DataFormatConstants.java
@@ -65,4 +65,8 @@ public class DataFormatConstants {
 			return toFormat;
 		}
 	}
+	
+	public static String formatMoney(final Long l) {
+		return new DecimalFormat(DataFormatConstants.MONEY_FORMAT_STRING).format(l);
+	}
 }
