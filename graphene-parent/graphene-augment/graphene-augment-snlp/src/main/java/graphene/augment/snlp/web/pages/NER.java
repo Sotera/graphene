3ba6c963b24f194bd33d980aa7c20843de142def
djue <djue@phy6.net>
2015-02-11 02:22:11 -0500

    Moved SNLP pages outside of an implementation

---
+++ graphene-parent/graphene-augment/graphene-augment-snlp/src/main/java/graphene/augment/snlp/web/pages/NER.java
@@ -0,0 +1,89 @@
+package graphene.augment.snlp.web.pages;
+
+import graphene.augment.snlp.model.NERResult;
+import graphene.augment.snlp.services.NERService;
+import graphene.model.idl.G_VisualType;
+import graphene.web.annotations.PluginPage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
+import org.apache.tapestry5.upload.services.UploadedFile;
+import org.slf4j.Logger;
+
+@PluginPage(visualType = G_VisualType.PLUGIN, menuName = "NER", icon = "fa fa-lg fa-fw fa-comments")
+@Import(library = { "context:core/js/plugin/dropzone/dropzone.js",
+		"context:/core/js/startdropzone.js" })
+public class NER {
+	@Inject
+	private AjaxResponseRenderer ajaxResponseRenderer;
+	@Property
+	private NERResult currentResult;
+	@Property
+	private UploadedFile file;
+	@Property
+	@Persist
+	private boolean highlightZoneUpdates;
+	@Property
+	private int index;
+	@InjectComponent
+	private Zone listZone;
+	@Component
+	private Form mydropzone;
+
+	@Inject
+	private Logger logger;
+
+	@Inject
+	private Request request;
+
+	@Persist
+	private List<NERResult> nerresults;
+
+	public List<NERResult> getResults() {
+		return nerresults;
+	}
+
+	@Inject
+	private NERService nerService;
+
+	public String getZoneUpdateFunction() {
+		return highlightZoneUpdates ? "highlight" : "show";
+	}
+
+	void setupRender() {
+
+	}
+
+	public Object onSuccess() {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				file.getStream()));
+		String line;
+		nerresults = new ArrayList<NERResult>();
+		try {
+			while ((line = br.readLine()) != null) {
+				logger.debug(line);
+				nerresults.addAll(nerService.getResults(line));
+			}
+			logger.debug("Done extracting entities");
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+
+		return request.isXHR() ? listZone.getBody() : null;
+	}
+}
---
+++ graphene-parent/graphene-augment/graphene-augment-snlp/src/main/java/graphene/augment/snlp/web/pages/Sentiment.java
@@ -0,0 +1,80 @@
+package graphene.augment.snlp.web.pages;
+
+import graphene.augment.snlp.model.SentimentResult;
+import graphene.augment.snlp.model.StringWithSentiment;
+import graphene.augment.snlp.services.SentimentAnalyzer;
+import graphene.model.idl.G_VisualType;
+import graphene.web.annotations.PluginPage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
+import org.apache.tapestry5.upload.services.UploadedFile;
+import org.slf4j.Logger;
+
+@PluginPage(visualType = G_VisualType.EXPERIMENTAL, menuName = "Sentiment", icon = "fa fa-lg fa-fw fa-comments")
+@Import(library = { "context:core/js/plugin/dropzone/dropzone.js",
+		"context:/core/js/startdropzone.js" })
+public class Sentiment {
+	@Inject
+	private Request request;
+	@Property
+	private UploadedFile file;
+	@Inject
+	private Logger logger;
+	@Inject
+	private SentimentAnalyzer sa;
+	@Property
+	private int index;
+	@Property
+	private StringWithSentiment currentSentiment;
+
+	public Object onSuccessFromMydropzone() {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				file.getStream()));
+		String line;
+		List<StringWithSentiment> list = new ArrayList<StringWithSentiment>();
+		try {
+			while ((line = br.readLine()) != null) {
+				logger.debug(line);
+				list.add(sa.findSentiment(line));
+			}
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+		sr = new SentimentResult("asdf", list);
+
+		if (request.isXHR()) {
+			logger.debug("Rendering AJAX response");
+			ajaxResponseRenderer.addRender(listZone);
+		}
+		return this;
+	}
+
+	public String getZoneUpdateFunction() {
+		return highlightZoneUpdates ? "highlight" : "show";
+	}
+
+	@Property
+	@Persist
+	private boolean highlightZoneUpdates;
+	@InjectComponent
+	private Zone listZone;
+	@Inject
+	private AjaxResponseRenderer ajaxResponseRenderer;
+	@Persist
+	@Property
+	private SentimentResult sr;
+}
