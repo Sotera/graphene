5679660d2b8f98b929402a899472caac8ea2766e
Eli Fajardo <eli.fajardo@soteradefense.com>
2015-02-14 12:32:16 -0500

    Updated MITIEModule to use mitie.properties

--- graphene-parent/graphene-augment/graphene-augment-mitie/src/main/java/org/graphene/augment/mitie/MITIEModule.java
+++ graphene-parent/graphene-augment/graphene-augment-mitie/src/main/java/org/graphene/augment/mitie/MITIEModule.java
@@ -1,7 +1,12 @@
 package org.graphene.augment.mitie;
 
 import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.OrderedConfiguration;
+import org.apache.tapestry5.ioc.annotations.Contribute;
 import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.ioc.internal.services.ClasspathResourceSymbolProvider;
+import org.apache.tapestry5.ioc.services.SymbolProvider;
+import org.apache.tapestry5.ioc.services.SymbolSource;
 
 public class MITIEModule {
 
@@ -14,10 +19,20 @@ public class MITIEModule {
 
 		// MITIE defaults (if no mitie.properties file is provided, or not in
 		// catalina.properties, etc)
-		configuration.add(BASIC_AUTH, "");// obviously just an
-											// example
-		configuration.add(BASE_URL, "http://localhost/mitie");// obviously just
-																// an example
+		configuration.add(BASIC_AUTH, "");// obviously just an example
+		configuration.add(BASE_URL, "http://localhost/mitie");// obviously just an example
+	}
+	
+	/**
+	 * Tell Tapestry to look for an MITIE properties file in the
+	 * WEB-INF/classes folder of the war.
+	 * 
+	 * @param configuration
+	 */
+	@Contribute(SymbolSource.class)
+	public void contributePropertiesFileAsSymbols(final OrderedConfiguration<SymbolProvider> configuration) {
+		configuration.add("MitieConfiguration", new ClasspathResourceSymbolProvider("mitie.properties"),
+				"after:SystemProperties", "before:ApplicationDefaults");
 	}
 
 	public MITIERestAPIConnection buildMITIEConnection(
