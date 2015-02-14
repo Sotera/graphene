ab0a8829b71be5dbe8841f884a562423571ae022
djue <djue@phy6.net>
2015-02-14 14:06:17 -0500

    Fixed findbugs error

--- graphene-parent/graphene-model/src/main/java/graphene/model/idlhelper/SolrUtils.java
+++ graphene-parent/graphene-model/src/main/java/graphene/model/idlhelper/SolrUtils.java
@@ -40,199 +40,197 @@ import java.util.regex.Pattern;
  */
 public class SolrUtils {
 
-	  /**
-	   * See org.apache.solr.client.solrj.util.ClientUtils
-	   * See: {@link org.apache.lucene.queryparser.classic queryparser syntax} 
-	   * for more information on Escaping Special Characters
-	   */
-	  public static String escapeQueryChars(String s) {
-	    StringBuilder sb = new StringBuilder();
-	    for (int i = 0; i < s.length(); i++) {
-	      char c = s.charAt(i);
-	      // These characters are part of the query syntax and must be escaped
-	      if (c == '\\' || c == '+' || c == '-' || c == '!'  || c == '(' || c == ')' || c == ':'
-	        || c == '^' || c == '[' || c == ']' || c == '\"' || c == '{' || c == '}' || c == '~'
-	        || c == '*' || c == '?' || c == '|' || c == '&'  || c == ';' || c == '/'
-	        || Character.isWhitespace(c)) {
-	        sb.append('\\');
-	      }
-	      sb.append(c);
-	    }
-	    return sb.toString();
-	  }
-
-	  
 	private static Pattern NUMBER_PATTERN = Pattern.compile("[0-9]");
+
 	private static Pattern TERM_SEPARATOR = Pattern.compile("\\.\\s+|[,;:\\?\\s]+");
+
+	/**
+	 * See org.apache.solr.client.solrj.util.ClientUtils See:
+	 * {@link org.apache.lucene.queryparser.classic queryparser syntax} for more
+	 * information on Escaping Special Characters
+	 */
+	public static String escapeQueryChars(final String s) {
+		final StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			final char c = s.charAt(i);
+			// These characters are part of the query syntax and must be escaped
+			if ((c == '\\') || (c == '+') || (c == '-') || (c == '!') || (c == '(') || (c == ')') || (c == ':')
+					|| (c == '^') || (c == '[') || (c == ']') || (c == '\"') || (c == '{') || (c == '}') || (c == '~')
+					|| (c == '*') || (c == '?') || (c == '|') || (c == '&') || (c == ';') || (c == '/')
+					|| Character.isWhitespace(c)) {
+				sb.append('\\');
+			}
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 
 	/**
 	 * Returns a Solr query clause to represent the descriptor supply.
 	 * 
 	 * @param descriptor
-	 * 		The match specification
+	 *            The match specification
 	 * 
 	 * @return
 	 */
-	public static String toSolrClause(G_PropertyMatchDescriptor descriptor) {
-		
-		String k = (String) descriptor.getKey();
-		
+	public static String toSolrClause(final G_PropertyMatchDescriptor descriptor) {
+
+		final String k = descriptor.getKey();
+
 		Collection<Object> values;
-	
+
 		final Object r = descriptor.getRange();
-		
+
 		if (r instanceof G_SingletonRange) {
-			values = Collections.singleton(((G_SingletonRange)r).getValue());
+			values = Collections.singleton(((G_SingletonRange) r).getValue());
 		} else if (r instanceof G_ListRange) {
-			values = ((G_ListRange)r).getValues();
-			
-		// TODO: account for bounded ranges
+			values = ((G_ListRange) r).getValues();
+
+			// TODO: account for bounded ranges
 		} else {
-			values = null; 
+			values = null;
 		}
-		
-	
+
 		final StringBuilder s = new StringBuilder();
-		
+
 		// fuzzy?
 		if (PropertyMatchDescriptorHelper.isExclusion(descriptor)) {
 			s.append("-");
 		}
-		
+
 		s.append(k);
-		
-		if (G_Constraint.FUZZY_PARTIAL_OPTIONAL.equals(descriptor.getConstraint()) || G_Constraint.FUZZY_REQUIRED.equals(descriptor.getConstraint())) {
+
+		if (((values != null) && G_Constraint.FUZZY_PARTIAL_OPTIONAL.equals(descriptor.getConstraint()))
+				|| G_Constraint.FUZZY_REQUIRED.equals(descriptor.getConstraint())) {
 			s.append(":(");
-			
-			for (Object v : values) {
-				
+
+			for (final Object v : values) {
+
 				// add each term separately
-				String wsTokens[] = TERM_SEPARATOR.split(v.toString());
-				
-				for (String token : wsTokens) {
-					if (token.indexOf('-') == -1 || NUMBER_PATTERN.matcher(token).find()) {
+				final String wsTokens[] = TERM_SEPARATOR.split(v.toString());
+
+				for (final String token : wsTokens) {
+					if ((token.indexOf('-') == -1) || NUMBER_PATTERN.matcher(token).find()) {
 						s.append(escapeQueryChars(token));
 						s.append("~");
-						if (descriptor.getSimilarity() != null && descriptor.getSimilarity() != 1.0)
+						if ((descriptor.getSimilarity() != null) && (descriptor.getSimilarity() != 1.0)) {
 							s.append(descriptor.getSimilarity());
+						}
 						s.append(" ");
 					} else {
-						String hyphenTokens[] = token.split("-");
-						for (String seg : hyphenTokens) {
+						final String hyphenTokens[] = token.split("-");
+						for (final String seg : hyphenTokens) {
 							s.append(escapeQueryChars(seg));
 							s.append("~");
-							if (descriptor.getSimilarity() != null && descriptor.getSimilarity() != 1.0)
+							if ((descriptor.getSimilarity() != null) && (descriptor.getSimilarity() != 1.0)) {
 								s.append(descriptor.getSimilarity());
+							}
 							s.append(" ");
 						}
 					}
 				}
 			}
-			
-			s.setLength(s.length()-1);
+
+			s.setLength(s.length() - 1);
 			s.append(")");
-			
+
 		} else { // not | required / equals
 			s.append(":(");
-			
-			for (Object v : values) {
-				s.append("\"");
-				s.append(v);
-				s.append("\" ");
+			if (values != null) {
+				for (final Object v : values) {
+					s.append("\"");
+					s.append(v);
+					s.append("\" ");
+				}
 			}
-			
-			s.setLength(s.length()-1);
+
+			s.setLength(s.length() - 1);
 			s.append(")");
 		}
-			
-	
-		if (descriptor.getWeight() != null && descriptor.getWeight() != 1.0) {
+
+		if ((descriptor.getWeight() != null) && (descriptor.getWeight() != 1.0)) {
 			s.append("^");
 			s.append(descriptor.getWeight());
 		}
-		
+
 		return s.toString();
 	}
 
 	/**
-	 * Returns an OR'd series of Solr clauses to represent the list of terms specified.
+	 * Returns an OR'd series of Solr clauses to represent the list of terms
+	 * specified.
 	 * 
 	 * @param basicQuery
-	 * 		A search over default fields supplied by basicQueryFieldWeights
+	 *            A search over default fields supplied by
+	 *            basicQueryFieldWeights
 	 * 
 	 * @param basicQueryFieldWeights
-	 * 		A map of default field names to weightings, where 1.0 is the default weight.
+	 *            A map of default field names to weightings, where 1.0 is the
+	 *            default weight.
 	 * 
 	 * @param advancedTerms
-	 * 		A list of explicitly defined terms
+	 *            A list of explicitly defined terms
 	 * 
-	 * @return
-	 * 		The solr query string or null if not a valid query.
+	 * @return The solr query string or null if not a valid query.
 	 */
-	public static String toSolrQuery(String basicQuery, Map<String, Double> basicQueryFieldWeights, List<G_PropertyMatchDescriptor> advancedTerms) {
-		
+	public static String toSolrQuery(final String basicQuery, final Map<String, Double> basicQueryFieldWeights,
+			final List<G_PropertyMatchDescriptor> advancedTerms) {
+
 		// copy terms to merge basic with advanced.
-		final List<G_PropertyMatchDescriptor> orsAnds = 
-				new ArrayList<G_PropertyMatchDescriptor>(advancedTerms.size());
-			final List<G_PropertyMatchDescriptor> nots = 
-				new ArrayList<G_PropertyMatchDescriptor>(advancedTerms.size());
-				
+		final List<G_PropertyMatchDescriptor> orsAnds = new ArrayList<G_PropertyMatchDescriptor>(advancedTerms.size());
+		final List<G_PropertyMatchDescriptor> nots = new ArrayList<G_PropertyMatchDescriptor>(advancedTerms.size());
+
 		// first add basic query terms.
-		if (basicQuery != null && !basicQuery.isEmpty()) {
+		if ((basicQuery != null) && !basicQuery.isEmpty()) {
 			final Object values = PropertyMatchDescriptorHelper.rangeFromBasicTerms(basicQuery);
-			
+
 			if (values != null) {
-				for (String key : basicQueryFieldWeights.keySet()) {
-					orsAnds.add(
-						G_PropertyMatchDescriptor.newBuilder()
-							.setConstraint(G_Constraint.OPTIONAL_EQUALS)
-							.setKey(key)
-							.setRange(values)
-							.build()
-					);
+				for (final String key : basicQueryFieldWeights.keySet()) {
+					orsAnds.add(G_PropertyMatchDescriptor.newBuilder().setConstraint(G_Constraint.OPTIONAL_EQUALS)
+							.setKey(key).setRange(values).build());
 				}
 			}
 		}
-		
-		
+
 		// now form it into a query
-		StringBuilder query = new StringBuilder();
-		
+		final StringBuilder query = new StringBuilder();
+
 		// separate ors from nots
-		for (G_PropertyMatchDescriptor term : advancedTerms) {
-			(PropertyMatchDescriptorHelper.isExclusion(term)? nots: orsAnds).add(term);
+		for (final G_PropertyMatchDescriptor term : advancedTerms) {
+			(PropertyMatchDescriptorHelper.isExclusion(term) ? nots : orsAnds).add(term);
 		}
-	
+
 		// not valid
 		if (orsAnds.isEmpty()) {
 			return null;
 		}
-	
+
 		// orsAnds
-		for (G_PropertyMatchDescriptor term : orsAnds) {
+		for (final G_PropertyMatchDescriptor term : orsAnds) {
 			query.append(toSolrClause(term));
-			if (term.getConstraint().equals(G_Constraint.OPTIONAL_EQUALS) || term.getConstraint().equals(G_Constraint.FUZZY_PARTIAL_OPTIONAL)) {
-				query.append(" OR ");	
+			if (term.getConstraint().equals(G_Constraint.OPTIONAL_EQUALS)
+					|| term.getConstraint().equals(G_Constraint.FUZZY_PARTIAL_OPTIONAL)) {
+				query.append(" OR ");
 			} else {
 				query.append(" AND ");
 			}
-			
+
 		}
-		
+
 		// trim last OR
 		query.setLength(query.length() - 4);
-	
+
 		// nots
 		if (!nots.isEmpty()) {
 			query.insert(0, '(');
 			query.append(')');
-			
-			for (G_PropertyMatchDescriptor term : nots) {
+
+			for (final G_PropertyMatchDescriptor term : nots) {
 				query.append(" AND ");
 				query.append(toSolrClause(term));
 			}
 		}
-		
+
 		return query.toString();
 	}
 }
