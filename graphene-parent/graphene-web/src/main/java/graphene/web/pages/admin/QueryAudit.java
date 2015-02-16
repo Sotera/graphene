be7a575872850292a543ce7f8c67c98d759c0aa9
djue <djue@phy6.net>
2015-02-15 19:16:29 -0500

    Added requires admin to query audit page

--- graphene-parent/graphene-web/src/main/java/graphene/web/pages/admin/QueryAudit.java
+++ graphene-parent/graphene-web/src/main/java/graphene/web/pages/admin/QueryAudit.java
@@ -4,6 +4,7 @@ import graphene.model.idl.G_VisualType;
 import graphene.web.annotations.PluginPage;
 import graphene.web.pages.SimpleBasePage;
 
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.tapestry5.annotations.Import;
 import org.got5.tapestry5.jquery.ImportJQueryUI;
 
@@ -11,6 +12,7 @@ import org.got5.tapestry5.jquery.ImportJQueryUI;
 		"context:core/js/plugin/datatables/media/css/TableTools.css" })
 @ImportJQueryUI(theme = "context:core/js/libs/jquery/jquery-ui-1.10.3.min.js")
 @PluginPage(visualType = G_VisualType.ADMIN, menuName = "Query Audit", icon = "fa fa-lg fa-fw fa-info-circle")
+@RequiresRoles("admin")
 public class QueryAudit extends SimpleBasePage {
 
 }
