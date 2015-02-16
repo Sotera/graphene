45f844d52bcd32e2b55446050fc60c5fd16a1096
djue <djue@phy6.net>
2015-02-16 05:21:01 -0500

    interface update

--- graphene-parent/graphene-dao-neo4j/src/main/java/graphene/dao/neo4j/UserDAONeo4JEImpl.java
+++ graphene-parent/graphene-dao-neo4j/src/main/java/graphene/dao/neo4j/UserDAONeo4JEImpl.java
@@ -19,15 +19,13 @@ import java.util.Map;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
 import org.slf4j.Logger;
 
-public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
-		UserDAO {
+public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements UserDAO {
 
 	// all the fields for any classes we can search on here
 	@Inject
@@ -39,21 +37,19 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 		// TODO Auto-generated constructor stub
 	}
 
-	public UserDAONeo4JEImpl(@UserGraph Neo4JEmbeddedService service) {
-		this.n4jService = service;
+	public UserDAONeo4JEImpl(@UserGraph final Neo4JEmbeddedService service) {
+		n4jService = service;
 	}
 
 	@Override
-	public long countUsers(String partialName) {
+	public long countUsers(final String partialName) {
 		long n = 0;
 		try (Transaction tx = beginTx()) {
-			String queryString = "start n = node(*) where n."
-					+ G_UserFields.username + " =~ '.*" + partialName
+			final String queryString = "start n = node(*) where n." + G_UserFields.username + " =~ '.*" + partialName
 					+ ".*' return count(*) as c;";
-			Map<String, Object> parameters = new HashMap<String, Object>();
-			ResourceIterator<Object> resultIterator = n4jService
-					.getExecutionEngine().execute(queryString, parameters)
-					.columnAs("c");
+			final Map<String, Object> parameters = new HashMap<String, Object>();
+			final ResourceIterator<Object> resultIterator = n4jService.getExecutionEngine()
+					.execute(queryString, parameters).columnAs("c");
 			n = (long) resultIterator.next();
 			tx.success();
 		}
@@ -61,18 +57,15 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 
 	}
 
-	private Node createUniqueUser(String username, String password) {
+	private Node createUniqueUser(final String username, final String password) {
 		Node result = null;
 		ResourceIterator<Node> resultIterator = null;
 		try (Transaction tx = beginTx()) {
-			String queryString = "MERGE (n:"
-					+ GrapheneNeo4JConstants.userLabel.name() + " {"
-					+ G_UserFields.username.name()
-					+ ": {theUserName}}) RETURN n";
-			Map<String, Object> parameters = new HashMap<>();
+			final String queryString = "MERGE (n:" + GrapheneNeo4JConstants.userLabel.name() + " {"
+					+ G_UserFields.username.name() + ": {theUserName}}) RETURN n";
+			final Map<String, Object> parameters = new HashMap<>();
 			parameters.put("theUserName", username);
-			resultIterator = n4jService.getExecutionEngine()
-					.execute(queryString, parameters).columnAs("n");
+			resultIterator = n4jService.getExecutionEngine().execute(queryString, parameters).columnAs("n");
 			result = resultIterator.next();
 			setPasswordHash(result, password);
 			resultIterator.close();
@@ -82,24 +75,24 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	}
 
 	@Override
-	public boolean delete(String id) {
+	public boolean delete(final String id) {
 		boolean success = false;
 		try (Transaction tx = beginTx()) {
-			Node u = getUserNodeById(id);
-			for (Relationship r : u.getRelationships()) {
+			final Node u = getUserNodeById(id);
+			for (final Relationship r : u.getRelationships()) {
 				r.delete();
 			}
 			u.delete();
 			tx.success();
 			success = true;
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.error(ExceptionUtil.getRootCauseMessage(e));
 		}
 		return success;
 	}
 
 	@Override
-	public boolean disable(String id) {
+	public boolean disable(final String id) {
 		try (Transaction tx = beginTx()) {
 			getUserNodeById(id).setProperty("active", false);
 			tx.success();
@@ -108,7 +101,7 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	}
 
 	@Override
-	public boolean enable(String id) {
+	public boolean enable(final String id) {
 		try (Transaction tx = beginTx()) {
 			getUserNodeById(id).setProperty("active", true);
 			tx.success();
@@ -118,19 +111,18 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 
 	@Override
 	public List<G_User> getAll() {
-		List<G_User> list = new ArrayList<G_User>();
+		final List<G_User> list = new ArrayList<G_User>();
 		try (Transaction tx = beginTx()) {
 			try (ResourceIterator<Node> iter = n4jService.getGgo()
-					.getAllNodesWithLabel(GrapheneNeo4JConstants.userLabel)
-					.iterator()) {
+					.getAllNodesWithLabel(GrapheneNeo4JConstants.userLabel).iterator()) {
 				while (iter.hasNext()) {
-					G_User d = userFunnel.from(iter.next());
+					final G_User d = userFunnel.from(iter.next());
 					if (d != null) {
 						list.add(d);
 					}
 				}
 			}
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.error(e.getMessage());
 			return null;
 		}
@@ -138,13 +130,13 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	}
 
 	@Override
-	public G_User getById(String id) {
-		Node n = getUserNodeById(id);
+	public G_User getById(final String id) {
+		final Node n = getUserNodeById(id);
 		G_User g = null;
 		try (Transaction tx = beginTx()) {
 			g = userFunnel.from(n);
 			tx.success();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.error(e.getMessage());
 
 		}
@@ -152,13 +144,11 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	}
 
 	@Override
-	public List<G_User> getByPartialUsername(String partialName, int offset,
-			int limit) {
-		List<G_User> list = new ArrayList<G_User>();
+	public List<G_User> getByPartialUsername(final String partialName, final int offset, final int limit) {
+		final List<G_User> list = new ArrayList<G_User>();
 
 		try (Transaction tx = beginTx()) {
-			String queryString = "start n = node(*) where n."
-					+ G_UserFields.username + " =~ '.*" + partialName
+			String queryString = "start n = node(*) where n." + G_UserFields.username + " =~ '.*" + partialName
 					+ ".*' return n order by n." + G_UserFields.username;
 
 			if (offset > 0) {
@@ -168,10 +158,9 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 				queryString += " limit " + limit;
 			}
 
-			Map<String, Object> parameters = new HashMap<String, Object>();
-			ResourceIterator<Object> resultIterator = n4jService
-					.getExecutionEngine().execute(queryString, parameters)
-					.columnAs("n");
+			final Map<String, Object> parameters = new HashMap<String, Object>();
+			final ResourceIterator<Object> resultIterator = n4jService.getExecutionEngine()
+					.execute(queryString, parameters).columnAs("n");
 			while (resultIterator.hasNext()) {
 				list.add(userFunnel.from((Node) resultIterator.next()));
 			}
@@ -187,30 +176,29 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	// }
 
 	@Override
-	public G_User getByUsername(String username) {
+	public G_User getByUsername(final String username) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String getPasswordHash(String id, String password) {
+	public String getPasswordHash(final String id, final String password) {
 		String hash = null;
 		try {
 			hash = passwordHasher.createHash(password);
 		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-			logger.error("Error getting password hash for id " + id+" : "+e.getMessage());
-		
+			logger.error("Error getting password hash for id " + id + " : " + e.getMessage());
+
 		}
 		return hash;
 	}
 
-	//@PostInjection
+	// @PostInjection
+	@Override
 	public void initialize() throws DataAccessException {
 		if (n4jService.connectToGraph()) {
-			logger.debug("Constructing UserDAONeo4JImpl hooked up to "
-					+ n4jService.getLocation());
-			n4jService.createNewIndex(GrapheneNeo4JConstants.userLabel,
-					G_UserFields.username.name());
+			logger.debug("Constructing UserDAONeo4JImpl hooked up to " + n4jService.getLocation());
+			n4jService.createNewIndex(GrapheneNeo4JConstants.userLabel, G_UserFields.username.name());
 
 		} else {
 			logger.error("Could not connect to graph database");
@@ -220,86 +208,46 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	}
 
 	@Override
-	public boolean isExisting(String username) {
-		return (getByUsername(username) == null) ? false : true;
-	}
-
-	@Override
-	public boolean isExistingId(String id) {
+	public boolean isExistingId(final String id) {
 		return (getUserNodeById(id) == null) ? false : true;
 	}
 
 	@Override
-	public G_User loginUser(String id, String password)
-			throws AuthenticationException {
-		Node node = getUserNodeById(id);
+	public boolean isExistingUsername(final String username) {
+		return (getByUsername(username) == null) ? false : true;
+	}
+
+	@Override
+	public G_User loginAuthenticatedUser(final String id) {
+		return userFunnel.from(getUserNodeById(id));
+	}
+
+	@Override
+	public G_User loginUser(final String id, final String password) throws AuthenticationException {
+		final Node node = getUserNodeById(id);
 		try (Transaction tx = beginTx()) {
 
 			if (node != null) {
-				String hash = (String) node
-						.getProperty(G_UserFields.hashedpassword.name());
+				final String hash = (String) node.getProperty(G_UserFields.hashedpassword.name());
 				try {
 					if (passwordHasher.validatePassword(password, hash)) {
 
-						node.setProperty(G_UserFields.lastlogin.name(),
-								DateTime.now(DateTimeZone.UTC).getMillis());
+						node.setProperty(G_UserFields.lastlogin.name(), DateTime.now().getMillis());
 					}
 				} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-					logger.error("Error logging in, could not validate password for "
-							+ id+ " : "+e.getMessage());
-				
+					logger.error("Error logging in, could not validate password for " + id + " : " + e.getMessage());
+
 				}
 			} else {
 				logger.error("No user with id " + id);
 			}
 			tx.success();
 
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.error(e.getMessage());
 			throw new AuthenticationException(e.getMessage());
 		}
 		return userFunnel.from(node);
-	}
-
-	/**
-	 * Note this behaves slightly differently than the non detached version.
-	 */
-	@Override
-	public G_User save(G_User d) {
-
-		G_User user = null;
-		if (!ValidationUtils.isValid(d.getUsername())) {
-			return null;
-		}
-		ResourceIterator<Node> resultIterator = null;
-		try (Transaction tx = beginTx()) {
-			String queryString = "MERGE (n:"
-					+ GrapheneNeo4JConstants.userLabel.name() + " {"
-					+ G_UserFields.username.name() + ": {var}}) RETURN n";
-			Map<String, Object> parameters = new HashMap<>();
-			parameters.put("var", d.getUsername());
-			resultIterator = n4jService.getExecutionEngine()
-					.execute(queryString, parameters).columnAs("n");
-
-			Node n = resultIterator.next();
-			n.setProperty(G_UserFields.created.name(), DateTime.now()
-					.getMillis());
-			n.setProperty(G_UserFields.active.name(), d.getActive());
-			n.setProperty(G_UserFields.avatar.name(), d.getAvatar());
-			n.setProperty(G_UserFields.email.name(), d.getEmail());
-			n.setProperty(G_UserFields.fullname.name(), d.getFullname());
-			n.setProperty(G_UserFields.lastlogin.name(), 0l);
-			n.setProperty(G_UserFields.numberlogins.name(), d.getNumberlogins());
-			n.setProperty(G_UserFields.username.name(), d.getUsername());
-
-			user = userFunnel.from(n);// TODO: test to make sure this has all
-										// the
-										// fields we want.
-			resultIterator.close();
-			tx.success();
-		}
-
-		return user;
 	}
 
 	// @Override
@@ -321,18 +269,56 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	// }
 	// }
 
-	public Node setPasswordHash(Node n, String password) {
+	/**
+	 * Note this behaves slightly differently than the non detached version.
+	 */
+	@Override
+	public G_User save(final G_User d) {
+
+		G_User user = null;
+		if (!ValidationUtils.isValid(d.getUsername())) {
+			return null;
+		}
+		ResourceIterator<Node> resultIterator = null;
+		try (Transaction tx = beginTx()) {
+			final String queryString = "MERGE (n:" + GrapheneNeo4JConstants.userLabel.name() + " {"
+					+ G_UserFields.username.name() + ": {var}}) RETURN n";
+			final Map<String, Object> parameters = new HashMap<>();
+			parameters.put("var", d.getUsername());
+			resultIterator = n4jService.getExecutionEngine().execute(queryString, parameters).columnAs("n");
+
+			final Node n = resultIterator.next();
+			n.setProperty(G_UserFields.created.name(), DateTime.now().getMillis());
+			n.setProperty(G_UserFields.active.name(), d.getActive());
+			n.setProperty(G_UserFields.avatar.name(), d.getAvatar());
+			n.setProperty(G_UserFields.email.name(), d.getEmail());
+			n.setProperty(G_UserFields.fullname.name(), d.getFullname());
+			n.setProperty(G_UserFields.lastlogin.name(), 0l);
+			n.setProperty(G_UserFields.numberlogins.name(), d.getNumberlogins());
+			n.setProperty(G_UserFields.username.name(), d.getUsername());
+
+			user = userFunnel.from(n);// TODO: test to make sure this has all
+										// the
+										// fields we want.
+			resultIterator.close();
+			tx.success();
+		}
+
+		return user;
+	}
+
+	public Node setPasswordHash(final Node n, final String password) {
 		// NOTE: Does not check to see if the password is a good one, only that
 		// it is set.
 		if (ValidationUtils.isValid(password)) {
 			// then update
 			try {
-				String hash = passwordHasher.createHash(password);
+				final String hash = passwordHasher.createHash(password);
 				logger.info("Updating password hash to " + hash);
 				n.setProperty(G_UserFields.hashedpassword.name(), hash);
 			} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-				logger.error("Error setting password hash for user "
-						+ n.getProperty(G_UserFields.username.name())+ " : "+e.getMessage());
+				logger.error("Error setting password hash for user " + n.getProperty(G_UserFields.username.name())
+						+ " : " + e.getMessage());
 			}
 
 		}
@@ -340,20 +326,15 @@ public class UserDAONeo4JEImpl extends GenericUserSpaceDAONeo4jE implements
 	}
 
 	@Override
-	public boolean updatePasswordHash(String id, String password) {
+	public boolean updatePasswordHash(final String id, final String password) {
 		try (Transaction tx = beginTx()) {
 			setPasswordHash(getUserNodeById(id), password);
 			tx.success();
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.error(e.getMessage());
 			return false;
 		}
 		return true;
-	}
-
-	@Override
-	public G_User loginAuthenticatedUser(String id) {
-		return userFunnel.from(getUserNodeById(id));
 	}
 
 }
