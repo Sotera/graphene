2370b74a1721544a4b68b476ac81fc1615f04ee5
djue <djue@phy6.net>
2015-02-15 19:17:41 -0500

    Updates to unavailable and page denied pages.

--- graphene-parent/graphene-web/src/main/java/graphene/web/pages/pub/PageDenied.java
+++ graphene-parent/graphene-web/src/main/java/graphene/web/pages/pub/PageDenied.java
@@ -1,39 +1,55 @@
-package graphene.web.pages.infrastructure;
+package graphene.web.pages.pub;
+
+import graphene.model.idl.G_SymbolConstants;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tapestry5.Asset;
+import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.services.Response;
 
-
 /**
- * Intended for use with PageProtectionFilter, this displays the path of the page to which you are not authorised.
+ * Intended for use with PageProtectionFilter, this displays the path of the
+ * page to which you are not authorized.
  */
 public class PageDenied {
 
-    // Activation context
+	@Inject
+	@Path("context:/core/img/logo_graphene_dark_wide.png")
+	@Property
+	private Asset imgLogo;
+	@Property
+	@Inject
+	@Symbol(G_SymbolConstants.APPLICATION_NAME)
+	private String appName;
 
-    @Property
-    private String urlDenied;
+	@Property
+	@Inject
+	@Symbol(G_SymbolConstants.APPLICATION_VERSION)
+	private String appVersion;
+	@Property
+	private String urlDenied;
 
-    // Other useful bits and pieces
+	// Other useful bits and pieces
 
-    @Inject
-    private Response response;
+	@Inject
+	private Response response;
 
-    // The code
+	// The code
 
-    String onPassivate() {
-        return urlDenied;
-    }
+	void onActivate(final String urlDenied) {
+		this.urlDenied = urlDenied;
+	}
 
-    void onActivate(String urlDenied) {
-        this.urlDenied = urlDenied;
-    }
+	String onPassivate() {
+		return urlDenied;
+	}
 
-    public void setupRender() {
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    }
+	public void setupRender() {
+		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+	}
 
 }
